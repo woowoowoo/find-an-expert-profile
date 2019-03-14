a52695590bf245a384230d26a116f08536dade30
Andrew Harris <andrew@woowoowoo.com>
2019-03-14 14:55:45 +1100

    maybe text

--- scraper.js
+++ scraper.js
@@ -45,7 +45,7 @@ function run(db) {
 		// Use cheerio to find things in the page with css selectors.
 		var $ = cheerio.load(body);
 
-		var elements = $("#photo-wrapper").each(function () {
+		var elements = $("#individual-info-unimelb header").each(function () {
 			var value = $(this).text().trim();
 			updateRow(db, value);
 		});
